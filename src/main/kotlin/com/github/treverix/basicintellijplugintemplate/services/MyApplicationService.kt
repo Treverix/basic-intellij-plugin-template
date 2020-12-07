7cf04b13b6e5a60956cb7e19f7f352ae8c6ee81d
GitHub Action <action@github.com>
2020-12-07 11:17:45 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/treverix/basicintellijplugintemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.treverix.basicintellijplugintemplate.services
+
+import com.github.treverix.basicintellijplugintemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
